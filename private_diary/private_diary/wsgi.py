e5fcfb878ff4ce616e18ba9c8cb574ebf8de0141
yuuki.kato <0me3210g039530z@gmail.com>
2022-04-30 16:09:21 +0900

    本番運用前　Chapter12 11節

--- private_diary/private_diary/wsgi.py
+++ private_diary/private_diary/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'private_diary.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'private_diary.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'private_diary.settings_dev')
 
 application = get_wsgi_application()
